aeb59a9b9be29d86551258ab44da4cf1375ca6c6
He Zhangke <42808483@qq.com>
2018-06-06 23:38:45 +0800

    modify ex1.py add code

--- ex1.py
+++ ex1.py
@@ -6,4 +6,13 @@ upper = []
 low = []
 for i in range(24):
 	low.append(chr(ord('a') + i + 2))
+	upper.append(chr(ord('A')+i+2))
+for j in range(2):
+	low.append(chr(ord('a')+j))
+	upper.append(chr(ord('A')+j))
+change = ''.join(low+upper)  
+upper  = maketrans('','')[65:91]
+low = maketrans('','')[97:123]
+source = low + upper
+print s.translate(maketrans(source,change))
 
